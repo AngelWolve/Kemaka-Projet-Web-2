54f07e52073abbbe3e6663498908ee4970db75f7
KevinLAubut <kevin.l.aubut@gmail.com>
2023-09-14 13:42:06 -0400

    Configurer "ActiviteFactory"

--- database/factories/ActiviteFactory.php
+++ database/factories/ActiviteFactory.php
@@ -14,7 +14,10 @@ class ActiviteFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'nom' => $this->faker->sentence(10),
+            'description' => $this->faker->text(500),
+            'image' => "https://placekitten.com/400/250?image=" . rand(1, 16),
+            'user_id' => rand(1, 10),
         ];
     }
 }

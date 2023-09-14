112751da3b56f6cc723da5a2f13866f5ee8ad125
KevinLAubut <kevin.l.aubut@gmail.com>
2023-09-14 14:41:55 -0400

    Configurer "ReservationFactory"

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Forfait;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ReservationFactory extends Factory
@@ -14,7 +16,10 @@ class ReservationFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'date_arrivee' => $this->faker->dateTimeBetween('now', '+1 years'),
+            'date_depart' => $this->faker->dateTimeBetween('+1 years', '+2 years'),
+            'user_id' => User::inRandomOrder()->first()->id,
+            'forfait_id' => Forfait::inRandomOrder()->first()->id,
         ];
     }
 }

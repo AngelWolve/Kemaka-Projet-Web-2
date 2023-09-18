6d8897c29624ebd73ff1b220f696af712f65b9a1
KevinLAubut <kevin.l.aubut@gmail.com>
2023-09-18 17:08:58 -0400

    Implémentation de connexion et de l'enregistrement

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,28 @@
 <?php
 
+use App\Http\Controllers\ConnexionController;
+use App\Http\Controllers\EnregistrementController;
 use Illuminate\Support\Facades\Route;
 
+/*****************
+ * CONNEXION ET ENREGISTREMENT
+ */
+Route::get("/connexion", [ConnexionController::class, 'create'])
+    ->name('connexion.create');
+// ->middleware('guest');
+
+Route::post("/connexion", [ConnexionController::class, 'authentifier'])
+    ->name('connexion.authentifier');
+// ->middleware('guest');
+
+Route::post("/deconnexion", [ConnexionController::class, 'deconnecter'])
+    ->name('deconnexion');
+// ->middleware('auth');
+
+Route::get("/enregistrement", [EnregistrementController::class, 'create'])
+    ->name('enregistrement.create');
+// ->middleware('guest');
+
+Route::post("/enregistrement", [EnregistrementController::class, 'store'])
+    ->name('enregistrement.store');
+    // ->middleware('guest');

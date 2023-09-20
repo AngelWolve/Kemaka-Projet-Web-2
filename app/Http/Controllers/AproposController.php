b249dd27a24a1e560f86c37d876da69745b3e919
KevinLAubut <kevin.l.aubut@gmail.com>
2023-09-20 12:08:41 -0400

    Implémentation du contrôleur "À propos"

---
+++ app/Http/Controllers/AproposController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AproposController extends Controller
+{
+    /**
+     * Affiche la page "À propos"
+     *
+     * @return void
+     */
+    public function index()
+    {
+        return view('apropos');
+    }
+}

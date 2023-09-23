e0612f5c1fdef6438c44d63c8f7074be74dbea12
LAPTOP-6FAK7V3D\Massomeh <mofidipoorm@yahoo.com>
2023-09-22 23:20:53 -0400

    Ajout blade <x-admin> dans le fichier actualites/ index.blade.php

--- resources/views/admin/actualites/index.blade.php
+++ resources/views/admin/actualites/index.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+<x-admin>
     <h1>Toutes les actualités</h1>
     <main>
         @if ($actualites->isEmpty())
@@ -63,6 +53,4 @@
             </ul>
         @endif
     </main>
-</body>
-
-</html>
+</x-admin>

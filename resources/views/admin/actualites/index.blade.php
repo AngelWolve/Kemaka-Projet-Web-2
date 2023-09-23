216febcf77a501cf5ab39e5c0de1c45b7f45fc9f
LAPTOP-6FAK7V3D\Massomeh <mofidipoorm@yahoo.com>
2023-09-23 16:15:14 -0400

    Mise à jour de scss pour la page:admin/actualites

--- resources/views/admin/actualites/index.blade.php
+++ resources/views/admin/actualites/index.blade.php
@@ -1,56 +1,78 @@
 <x-admin>
-    <h1>Toutes les actualités</h1>
-    <main>
+
+    <main class="actualite">
+
         @if ($actualites->isEmpty())
             <h2>
                 Aucune actualités actuellement
             </h2>
         @else
             {{-- Liste des actualités --}}
-            <ul>
-                @foreach ($actualites as $actualite)
-                    <li>
-                        <h2>
-                            {{ $actualite->titre }}
-                        </h2>
-                        <p>
-                            {{ $actualite->description }}
-                        </p>
+            <section>
+                <ul>
+                    <h3>Toutes les actualités</h3>
+                   <div>
+                     {{-- CRÉATION --}}
+                     <a href="{{ route('admin/actualites.create') }}">
+                        <span>
+                            créer
+                        </span>
+                    </a>
+                     {{-- RETOUR --}}
+                     <a href="{{ route('admin.index') }}">
+                        <span>
+                            Retour
+                        </span>
+                    </a>
 
-                        <p>
-                            {{ $actualite->created_at }}
-                        </p>
+                   </div>
 
-                        <div>
-                            <div>
-                                {{-- MODIFICATION --}}
-                                <a href="{{ route('admin/actualites.edit', ['id' => $actualite->id]) }}">
-                                    <span>
-                                        edit
-                                    </span>
-                                </a>
-                                {{-- CRÉATION --}}
-                                <a href="{{ route('admin/actualites.create') }}">
-                                    <span>
-                                        créer
-                                    </span>
-                                </a>
-                                {{-- SUPPRESSION --}}
-                                <form action="{{ route('admin/actualites.destroy') }}" method="POST">
-                                    @csrf
+                    <div>
+                        @foreach ($actualites as $actualite)
+                            <li>
+                                <div>
+                                    <h4>
+                                        {{ $actualite->titre }}
+                                    </h4>
+                                    <p>
+                                        {{ $actualite->description }}
+                                    </p>
 
-                                    <input type="hidden" name="id" value="{{ $actualite->id }}">
-                                    <button type="submit">
-                                        <span>
-                                            delete
-                                        </span>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
+                                    <p>
+                                        {{ $actualite->created_at }}
+                                    </p>
+
+                                    <div>
+
+                                        {{-- MODIFICATION --}}
+                                        <a href="{{ route('admin/actualites.edit', ['id' => $actualite->id]) }}">
+                                            <span>
+                                                edit
+                                            </span>
+                                        </a>
+
+                                        {{-- SUPPRESSION --}}
+                                        <form action="{{ route('admin/actualites.destroy') }}" method="POST">
+                                            @csrf
+
+                                            <input type="hidden" name="id" value="{{ $actualite->id }}">
+                                            <button type="submit">
+                                                <span>
+                                                    delete
+                                                </span>
+                                            </button>
+                                        </form>
+                                    </div>
+
+                                </div>
+
+                            </li>
+                        @endforeach
+                    </div>
+
+                </ul>
+            </section>
+
         @endif
     </main>
 </x-admin>

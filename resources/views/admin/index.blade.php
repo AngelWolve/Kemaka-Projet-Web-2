356bcfefc7b6d38dad83d6bbea2ed23d0c632af6
LAPTOP-6FAK7V3D\Massomeh <mofidipoorm@yahoo.com>
2023-09-24 20:15:03 -0400

    Mise à jour le fichier _admin.scss

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,11 +1,11 @@
 <x-admin>
 
     <div class="container">
-        <a href="{{ route('admin/administrateurs.index') }}"> la liste des administateurs </a>
-        <a href="{{ route('admin/clients.index') }}"> la liste des clients </a>
-        <a href="{{ route('admin/employes.index') }}"> la liste des employés </a>
-        <a href="{{ route('admin/activites.index') }}"> la liste des activités </a>
-        <a href="{{ route('admin/actualites.index') }}"> la liste des actualités </a>
+        <div class="item"> <a href="{{ route('admin/administrateurs.index') }}"> la liste des administateurs </a> </div>
+        <div class="item"> <a href="{{ route('admin/clients.index') }}"> la liste des clients </a> </div>
+        <div class="item"> <a href="{{ route('admin/employes.index') }}"> la liste des employés </a> </div>
+        <div class="item"> <a href="{{ route('admin/activites.index') }}"> la liste des activités </a> </div>
+        <div class="item"> <a href="{{ route('admin/actualites.index') }}"> la liste des actualités </a> </div>
     </div>
 
 

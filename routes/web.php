1b2cf43422f5f0e04dcce51a7306b7def16559e6
KevinLAubut <kevin.l.aubut@gmail.com>
2023-09-26 15:46:21 -0400

    Ajout des "middlewares"

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
  */
 Route::get("/", [AccueilController::class, 'accueil'])
     ->name('accueil');
+    // ->middleware(['auth', 'admin']);
 
 /*****************
  * ADMIN

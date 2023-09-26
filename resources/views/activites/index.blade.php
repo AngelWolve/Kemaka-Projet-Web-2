b78c7050a6198b72588b74ac28c07e53daeb403b
LAPTOP-5CKH5DJA\archa <archangel14@live.ca>
2023-09-26 15:57:35 -0400

    ajout de components et changement de balises

--- resources/views/activites/index.blade.php
+++ resources/views/activites/index.blade.php
@@ -1,24 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Liste d'activités</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-
-<body>
-
-    <header>
-        <h1>Les activités</h1>
-    </header>
-
-    <main class="main-acitvites">
-        <section class="activites">
+<x-client titre="Nos activités">
+    <section id="activites">
+        <ul class="activites">
             @foreach ($activites as $activite)
-                <div class="activite">
+                <li class="activite">
                     <div class="image">
                         <img src="{{ $activite->image }}" alt="image">
                     </div>
@@ -30,11 +14,9 @@
                             {{ $activite->description }}
                         </p>
                     </div>
-                </div>
+                </li>
             @endforeach
-        </section>
-    </main>
-
-</body>
-
-</html>
+            </ul>
+            <x-parts.bouton-primaire route="forfaits.index" texte="Voir nos forfaits" />
+    </section>
+</x-client>

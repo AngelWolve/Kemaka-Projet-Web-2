11261f82a747618a4072ce871ff8c78f40cd5c84
KevinLAubut <kevin.l.aubut@gmail.com>
2023-09-27 10:04:00 -0400

    Mise à jour du SCSS

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+<x-client>
+    <h1>ERREUR 403!</h1>
+    <h2>Vous n'avez pas les droits pour accéder à cette page</h2>
+    <x-parts.button route="accueil" texte="Retour à l'accueil" />
+</x-client>

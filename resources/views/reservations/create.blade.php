d695e7363600f522d13a73684415803225b29c7b
LAPTOP-5CKH5DJA\archa <archangel14@live.ca>
2023-09-27 12:28:21 -0400

    ajouter un div et le input hidden de forfait_id

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -1,82 +1,49 @@
-{{-- <x-client titre="Réservez ici!">
-    <section id="reservation">
-        <h3>
-            {{ $forfait->nom }}
-        </h3>
-        <div class="prix">
-            <p class="prix-p tx-md">
-                <span>
-                    Prix:
-                </span>
-                {{ $forfait->prix }} $
-            </p>
-        </div>
-        <div class="duree">
-            <p class="duree-p tx-md">
-                <span>
-                    Durée:
-                </span>
-                {{ $forfait->duree }}
-            </p>
-    </section> --}}
-
-{{-- <section id="reservation-form">
-        <form action="{{ route('reservations.store') }}" method="post">
-            @csrf
-            <input type="hidden" name="forfait_id" value="{{ $forfait->id }}">
-            <label for="date_arrivee">Date d'arrivée :</label>
-            <input type="date" name="date_arrivee" required>
-            <label for="date_depart">Date de départ :</label>
-            <input type="date" name="date_depart" required>
-            <button type="submit">Réserver</button>
-        </form>
-    </section> --}}
-
 <x-client titre="Réservez ici!">
     <section id="reservations">
 
-        <div class="details">
-            <h3>
-                {{ $forfait->nom }}
-            </h3>
-            <div class="prix">
-                <p class="prix-p tx-md">
-                    <span>
-                        Prix:
-                    </span>
-                    {{ $forfait->prix }} $
-                </p>
+        <div class="reservations-bg">
+            <div class="details">
+                <h3>
+                    {{ $forfait->nom }}
+                </h3>
+                <div class="prix">
+                    <p class="prix-p tx-md">
+                        <span>
+                            Prix:
+                        </span>
+                        {{ $forfait->prix }} $
+                    </p>
+                </div>
+                <div class="duree">
+                    <p class="duree-p tx-md">
+                        <span>
+                            Durée:
+                        </span>
+                        {{ $forfait->duree }}
+                    </p>
+                </div>
             </div>
-            <div class="duree">
-                <p class="duree-p tx-md">
-                    <span>
-                        Durée:
-                    </span>
-                    {{ $forfait->duree }}
-                </p>
-            </div>
+            <form action="{{ route('reservations.store') }}" method="post">
+                @csrf
+                <div class="date">
+                    <label for="date_arrivee">Date d'arrivée :</label>
+                    <select name="date_arrivee" id="date_arrivee">
+                        @foreach ($datesDisponibleArrivee as $date)
+                            <option value="{{ $date }}">{{ $date }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="date">
+                    <label for="date_depart">Date de départ :</label>
+                    <select name="date_depart" id="date_depart">
+                        @foreach ($datesDisponibleDepart as $date)
+                            <option value="{{ $date }}">{{ $date }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <input type="hidden" name="forfait_id" value="{{ $forfait->id }}">
+                <button class="btn-primaire" type="submit">Réserver</button>
+            </form>
         </div>
-
-        <form action="{{ route('reservations.store') }}" method="post">
-            @csrf
-            <div class="date">
-                <label for="date_arrivee">Date d'arrivée :</label>
-                <select name="date_arrivee" id="date_arrivee">
-                    @foreach ($datesDisponibleArrivee as $date)
-                        <option value="{{ $date }}">{{ $date }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="date">
-                <label for="date_depart">Date de départ :</label>
-                <select name="date_depart" id="date_depart">
-                    @foreach ($datesDisponibleDepart as $date)
-                        <option value="{{ $date }}">{{ $date }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <button class="btn-primaire" type="submit">Réserver</button>
-        </form>
     </section>
 </x-client>

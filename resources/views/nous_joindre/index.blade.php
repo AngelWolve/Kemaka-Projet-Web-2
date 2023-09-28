8767899ffbf316e43188913f9d28cf941291b578
DESKTOP-QM4MQ9U\kevin <kevin.l.aubut@gmail.com>
2023-09-27 22:17:10 -0400

    Mise à jour SCSS

--- resources/views/nous_joindre/index.blade.php
+++ resources/views/nous_joindre/index.blade.php
@@ -36,7 +36,7 @@
             <p class="tx-md">info@asianquest.qc.ca</p>
         </div>
     </div>
-    <div class="cart">
+    <div class="carte">
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d357264.0014157304!2d-74.1116398966804!3d45.60813172715947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91a5a66841509%3A0x6434041e124a4c53!2sPalais%20des%20congr%C3%A8s%20de%20Montr%C3%A9al!5e0!3m2!1sfr!2sca!4v1695309596571!5m2!1sfr!2sca"
             width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"

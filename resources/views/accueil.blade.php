627c66eb56dd61c643fe9767edf0fa12e30980f9
DESKTOP-QM4MQ9U\kevin <kevin.l.aubut@gmail.com>
2023-10-01 10:58:50 -0400

    Mise à jour du SCSS

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,3 +1,21 @@
 <x-client>
+    <h1>
+        Accueil
+    </h1>
 
+    <section id="carrousel-instagram">
+        <h2>
+            Notre galerie Instagram
+        </h2>
+    </section>
+    <section id="carrousel-actualites">
+        <h2>
+            Les dernières actualités
+        </h2>
+    </section>
+    <section id="partenaires">
+        <h2>
+            Merci à nos partenaires
+        </h2>
+    </section>
 </x-client>

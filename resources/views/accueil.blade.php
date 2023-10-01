53be4e04dcccdc622ec78633f645cd06d690410d
DESKTOP-QM4MQ9U\kevin <kevin.l.aubut@gmail.com>
2023-10-01 11:14:33 -0400

    Mise à jour de l'accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,21 +1,34 @@
 <x-client>
-    <h1>
-        Accueil
-    </h1>
+    <div class="conteneur-accueil">
+        <h1>
+            Accueil
+        </h1>
 
-    <section id="carrousel-instagram">
-        <h2>
-            Notre galerie Instagram
-        </h2>
-    </section>
-    <section id="carrousel-actualites">
-        <h2>
-            Les dernières actualités
-        </h2>
-    </section>
-    <section id="partenaires">
-        <h2>
-            Merci à nos partenaires
-        </h2>
-    </section>
+        <section id="carrousel-instagram">
+            <h2>
+                Notre galerie Instagram
+            </h2>
+            <p>
+                Carrousel d'images ICI
+            </p>
+        </section>
+        <section id="carrousel-actualites">
+            <h2>
+                Les dernières actualités
+            </h2>
+            <p>
+                Carrousel d'actualités ICI
+            </p>
+        </section>
+        <section id="partenaires">
+            <h2>
+                Merci à nos partenaires
+            </h2>
+            <div class="partenaires">
+                <img src="{{ asset('images/logo-pipablogs.png') }}" alt="Logo PipaBlogs">
+                <img src="{{ asset('images/logo-freeair.png') }}" alt="Logo FreeAir">
+                <img src="{{ asset('images/logo-chicogames.png') }}" alt="Logo ChicoGames">
+            </div>
+        </section>
+    </div>
 </x-client>

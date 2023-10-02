0d9a48494d5a32f2b45db45509400c1d1a06735b
LAPTOP-5CKH5DJA\archa <archangel14@live.ca>
2023-10-02 16:35:54 -0400

    ajout du code de vue de la galerie instagram accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,3 +1,53 @@
 <x-client>
 
+    <section id="accueil">
+        <h2>Notre galerie Instagram</h2>
+
+        <div class="imgBox"><img src="images/insta_img1.png" alt=""></div>
+
+        <ul class="thumbnails">
+            <li>
+                <a href="images/insta_img1.png" target="imgBox">
+                    <img src="images/insta_img1.png" alt="" width="184">
+                </a>
+            </li>
+            <li>
+                <a href="images/insta_img2.png" target="imgBox">
+                    <img src="images/insta_img2.png" alt="" width="184">
+                </a>
+            </li>
+            <li>
+                <a href="images/insta_img3.png" target="imgBox">
+                    <img src="images/insta_img3.png" alt="" width="184">
+                </a>
+            </li>
+            <li>
+                <a href="images/insta_img4.png" target="imgBox">
+                    <img src="images/insta_img4.png" alt="" width="184">
+                </a>
+            </li>
+            <li>
+                <a href="images/insta_img5.png" target="imgBox">
+                    <img src="images/insta_img5.png" alt="" width="184">
+                </a>
+            </li>
+            <li>
+                <a href="images/insta_img6.png" target="imgBox">
+                    <img src="images/insta_img6.png" alt="" width="184">
+                </a>
+            </li>
+        </ul>
+    </section>
+
+
+    <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.thumbnails a').click(function(e) {
+                e.preventDefault();
+                $('.imgBox img').attr("src", $(this).attr("href"))
+            })
+        })
+    </script>
+
 </x-client>

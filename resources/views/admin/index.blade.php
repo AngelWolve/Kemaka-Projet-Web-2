09e6b252d7aa81ba9e9d69d8c12beae3258b555e
DESKTOP-QM4MQ9U\kevin <kevin.l.aubut@gmail.com>
2023-10-07 21:31:48 -0400

    Mise à jour SCSS

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,31 +3,21 @@
     <x-parts.alertes cle="succes" />
     <section id="admin">
         <div class="container">
-            <div class="item">
-                <a href="{{ route('admin/utilisateurs.index') }}">
-                    Liste des utilisateurs
-                </a>
-            </div>
-            <div class="item">
-                <a href="{{ route('admin/activites.index') }}">
-                    Liste des activités
-                </a>
-            </div>
-            <div class="item">
-                <a href="{{ route('admin/actualites.index') }}">
-                    Liste des actualités
-                </a>
-            </div>
-            <div class="item">
-                <a href="{{ route('reservations.index') }}">
-                    Liste des réservations
-                </a>
-            </div>
-            <div class="item">
-                <a href="{{ route('accueil') }}">
-                    Accueil
-                </a>
-            </div>
+            <a class="item" href="{{ route('admin/utilisateurs.index') }}">
+                Liste des utilisateurs
+            </a>
+            <a class="item" href="{{ route('admin/activites.index') }}">
+                Liste des activités
+            </a>
+            <a class="item" href="{{ route('admin/actualites.index') }}">
+                Liste des actualités
+            </a>
+            <a class="item" href="{{ route('reservations.index') }}">
+                Liste des réservations
+            </a>
+            <a class="item" href="{{ route('accueil') }}">
+                Accueil
+            </a>
         </div>
     </section>
 </x-admin>

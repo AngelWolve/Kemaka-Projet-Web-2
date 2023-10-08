ac0b5ce720b21b4ff79f572f884a9f0bedcccec1
DESKTOP-QM4MQ9U\kevin <kevin.l.aubut@gmail.com>
2023-10-08 16:57:49 -0400

    Mise à jour input, SCSS et documentation

--- resources/views/admin/utilisateurs/edit.blade.php
+++ resources/views/admin/utilisateurs/edit.blade.php
@@ -1,76 +1,86 @@
 <x-admin>
+    {{-- Section modification utilisateur --}}
     <section id="admin-modifier-utilisateur">
-        <div class=" modifier-form ">
+
+        {{-- Message de confirmation --}}
+        @if (session('succes'))
+            <p>
+                {{ session('succes') }}
+            </p>
+        @endif
+
+        <div class="modifier-utilisateur">
             <h1>Modifier un utilisateur</h1>
-            {{-- MESSAGES --}}
-            @if (session('succes'))
-                <p>
-                    {{ session('succes') }}</p>
-            @endif
-            {{-- FORMULAIRE --}}
+
+            {{-- Formulaire de modification --}}
             <form action="{{ route('admin/utilisateurs.update') }}" method="POST">
                 @csrf
 
-                <input type="hidden" name="id" value="{{ $utilisateur->id }}">
+                {{-- Champ caché pour l'id --}}
+                <x-forms.erreur champ="id" />
+                <input type="hidden" name="id" value="{{ $utilisateur->id }}" required>
 
-                {{-- PRÉNOM --}}
-                <div class="prenom">
-                    <label for="prenom">Prénom</label>
+                {{-- Prénom --}}
+                <div class="input">
+                    <label for="prenom">
+                        Prénom
+                    </label>
                     <div>
                         <x-forms.erreur champ="prenom" />
-
-                        <input id="prenom" name="prenom" type="text" autofocus
-                            value="{{ old('prenom') ?? $utilisateur->prenom }}">
+                        <input id="prenom" name="prenom" type="text" autofocus maxlength="255"
+                            value="{{ old('prenom') ?? $utilisateur->prenom }}" required>
                     </div>
                 </div>
 
-                {{-- NOM --}}
-                <div class="nom">
+                {{-- Nom --}}
+                <div class="input">
                     <label for="nom">Nom</label>
                     <div>
                         <x-forms.erreur champ="nom" />
-
-                        <input id="nom" name="nom" type="text" autofocus
-                            value="{{ old('nom') ?? $utilisateur->nom }}">
+                        <input id="nom" name="nom" type="text" autofocus maxlength="255"
+                            value="{{ old('nom') ?? $utilisateur->nom }}" required>
                     </div>
                 </div>
 
-                {{-- COURRIEL --}}
-                <div class="courriel">
+                {{-- Courriel --}}
+                <div class="input">
                     <label for="email">Courriel</label>
                     <div>
                         <x-forms.erreur champ="email" />
-
-                        <input id="email" name="email" type="text" autofocus
-                            value="{{ old('email') ?? $utilisateur->email }}">
+                        <input id="email" name="email" type="text" autofocus maxlength="255"
+                            value="{{ old('email') ?? $utilisateur->email }}" required>
                     </div>
                 </div>
 
-                {{-- ROLE --}}
-                <div class="role">
+                {{-- Rôle --}}
+                <div class="input">
                     <label for="role_id">Role</label>
                     <div>
-                        <select name="role_id" id="role_id">
+                        <x-forms.erreur champ="role_id" />
+                        <select name="role_id" id="role_id" required>
                             @foreach ($roles as $role)
                                 <option value="{{ $role->id }}" @if ($role->id == (old('role_id') ?? $utilisateur->role_id)) selected @endif>
-                                    {{ $role->nom }}
+                                    {{ ucfirst($role->nom) }}
                                 </option>
                             @endforeach
                         </select>
                     </div>
-
                 </div>
 
-                {{-- SUBMIT --}}
-                <div class="submit-modifier">
-                    <input class="btn-primaire" type="submit" value="Modifier!">
+                {{-- Soumission --}}
+                <div class="submit">
+                    <input class="btn-primaire" type="submit" value="Modifier">
                 </div>
-            </form>
 
-            {{-- RETOUR AUX LISTE --}}
-            <p class="retour-admin">
-                <a href="{{ route('admin/utilisateurs.index') }}">Retour à la Liste</a>
-            </p>
+            </form> {{-- Fin du formulaire de modification --}}
+
+            {{-- Retour --}}
+            <div class="retour">
+                <a href="{{ route('admin/utilisateurs.index') }}">
+                    Retour à la Liste
+                </a>
+            </div>
+
         </div>
-    </section>
+    </section> {{-- Fin de la section modifier un utilisateur --}}
 </x-admin>

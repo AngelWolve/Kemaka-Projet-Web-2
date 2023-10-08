e62ceaa11cc16e01af4aa6e24668e3cf3519a26a
DESKTOP-QM4MQ9U\kevin <kevin.l.aubut@gmail.com>
2023-10-08 17:18:50 -0400

    Mise à jour de la documentation

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,23 +1,30 @@
 <x-admin>
     {{-- Message de confirmation --}}
     <x-parts.alertes cle="succes" />
+
+    {{-- Section administration --}}
     <section id="admin">
         <div class="container">
+            {{-- Utilisateurs --}}
             <a class="item" href="{{ route('admin/utilisateurs.index') }}">
                 Liste des utilisateurs
             </a>
+            {{-- Activités --}}
             <a class="item" href="{{ route('admin/activites.index') }}">
                 Liste des activités
             </a>
+            {{-- Actualités --}}
             <a class="item" href="{{ route('admin/actualites.index') }}">
                 Liste des actualités
             </a>
+            {{-- Réservations --}}
             <a class="item" href="{{ route('reservations.index') }}">
                 Liste des réservations
             </a>
+            {{-- Retour à l'accueil --}}
             <a class="item" href="{{ route('accueil') }}">
                 Accueil
             </a>
         </div>
-    </section>
+    </section> {{-- Fin de la section administration --}}
 </x-admin>

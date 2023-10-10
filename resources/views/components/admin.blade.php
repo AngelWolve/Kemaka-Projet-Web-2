608ec3aab125fb8f01e9b465d2990681d409f470
DESKTOP-QM4MQ9U\kevin <kevin.l.aubut@gmail.com>
2023-10-09 22:52:04 -0400

    Ajout de google analytics

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -14,6 +14,18 @@
     @else
         <title>Asian Quest | Admin | {{ $titre }}</title>
     @endif
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RB3DNP3N8C"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-RB3DNP3N8C');
+    </script>
     <link rel="stylesheet" href="https://use.typekit.net/dcs5msy.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
--- resources/views/components/client.blade.php
+++ resources/views/components/client.blade.php
@@ -8,6 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Asian Quest | {{ $titre }}</title>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RB3DNP3N8C"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-RB3DNP3N8C');
+    </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://use.typekit.net/dcs5msy.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
